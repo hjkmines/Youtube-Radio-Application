d68f59224e1822034c69265eb9d5c1b5e8119efa
Tony Kim <hjkmines@gmail.com>
2020-07-27 13:23:01 -0600

    Initial commit

---
+++ Backend/config/environment.rb
@@ -0,0 +1,9 @@
+# Load the Rails application.
+require_relative 'application'
+require 'json'
+require 'rest-client'
+require 'dotenv'
+
+
+# Initialize the Rails application.
+Rails.application.initialize!
