2d790df7ed52b6e2a5dcacb12b5f5bd0c3eae854
Tony Kim <hjkmines@gmail.com>
2020-08-02 15:54:27 -0600

    adds fetch call to front end

--- Frontend/home.js
+++ Frontend/home.js
@@ -1,4 +1,4 @@
-const newsUrl = "http://localhost:3000/articles"
+const newsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2a717d771d85444cb9cb8eda83414b88"
 const imagesUrl = "http://localhost:3000/images"
 const stocksUrl = "http://localhost:3000/stocks"
 const $newsCarousel = document.querySelector(".carousel-news")
@@ -20,32 +20,32 @@ const $newsRow = document.querySelector(".news-row")
 
 fetch(newsUrl)
   .then(response => response.json())
-  .then(articles => {
+  .then(response => {
 
     for (i=0; i < 1; i++) {
       const $div = document.createElement("div")
       $div.classList.add("carousel-item")
       $div.classList.add("active")
-      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${articles[i].url}">${articles[i].title}</a></div><img class="d-block w-100 articles" src="${articles[i].image}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
+      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
       $newsCarousel.append($div)
     }
   
     for (i=1; i < 10; i++) {
       const $div = document.createElement("div")
       $div.classList.add("carousel-item")
-      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${articles[i].url}">${articles[i].title}</a></div><img class="d-block w-100 articles" src="${articles[i].image}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
+      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
       $newsCarousel.append($div)
     }
 
     for (i=10; i < 15; i++) {
       const $li = document.createElement("li")
-      $li.innerHTML = `<a id="headlines-linkers" href="${articles[i].url}"><strong>${articles[i].title}</strong></a><div class="headlines-bar"></div>`
+      $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a><div class="headlines-bar"></div>`
       $headLines.append($li)
     }
 
     for (i=15; i < 16; i++) {
       const $li = document.createElement("li")
-      $li.innerHTML = `<a id="headlines-linkers" href="${articles[i].url}"><strong>${articles[i].title}</strong></a>`
+      $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a>`
       $headLines.append($li)
     }
 
@@ -53,10 +53,10 @@ fetch(newsUrl)
 
       const $div = document.createElement('div')
       $div.classList.add('card')
-      $div.innerHTML = `<img class="card-img-top card-image" src="${articles[i].image}" alt="Card image cap">
+      $div.innerHTML = `<img class="card-img-top card-image" src="${response.articles[i].urlToImage}" alt="Card image cap">
       <div class="card-body">
-        <h5 class="card-title article-title">${articles[i].title}</h5>
-        <section class="article-button"><a href="${articles[i].url}" class="btn btn-primary access">Go to full article</a></section>
+        <h5 class="card-title article-title">${response.articles[i].title}</h5>
+        <section class="article-button"><a href="${response.articles[i].url}" class="btn btn-primary access">Go to full article</a></section>
       </div>`
       $newsRow.append($div)
     }
