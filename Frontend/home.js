de0da72ba8af7fa358561198f24e9e3e55d84dec
Tony Kim <hjkmines@gmail.com>
2020-08-02 16:58:24 -0600

    adds videos on first row

--- Frontend/home.js
+++ Frontend/home.js
@@ -7,93 +7,64 @@ const $headLines = document.querySelector(".headlines")
 const $stockList = document.querySelector(".stock-list")
 
 const $newsRow = document.querySelector(".news-row")
-
-// const CLIENT_ID = '135244444205-94826goosl58aslig9rd6g6tf71phlej.apps.googleusercontent.com'; 
-// const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
-// const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly'; 
-// const authorizeButton = document.getElementById('authorize-button'); 
-// const signoutButton = doucment.getElementById('signout-button');  
+const $title = document.querySelector('title')
 
 
 
-
-
-fetch(newsUrl)
-  .then(response => response.json())
-  .then(response => {
-
-    for (i=0; i < 1; i++) {
-      const $div = document.createElement("div")
-      $div.classList.add("carousel-item")
-      $div.classList.add("active")
-      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
-      $newsCarousel.append($div)
-    }
-  
-    for (i=1; i < 10; i++) {
-      const $div = document.createElement("div")
-      $div.classList.add("carousel-item")
-      $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
-      $newsCarousel.append($div)
-    }
-
-    for (i=10; i < 15; i++) {
-      const $li = document.createElement("li")
-      $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a><div class="headlines-bar"></div>`
-      $headLines.append($li)
-    }
-
-    for (i=15; i < 16; i++) {
-      const $li = document.createElement("li")
-      $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a>`
-      $headLines.append($li)
-    }
-
-    for (i=10; i < 15; i++) {
-
-      const $div = document.createElement('div')
-      $div.classList.add('card')
-      $div.innerHTML = `<img class="card-img-top card-image" src="${response.articles[i].urlToImage}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title article-title">${response.articles[i].title}</h5>
-        <section class="article-button"><a href="${response.articles[i].url}" class="btn btn-primary access">Go to full article</a></section>
-      </div>`
-      $newsRow.append($div)
-    }
-
-  })
-
-  fetch(stocksUrl)
+if ($title.textContent == "Home") {
+  fetch(newsUrl)
     .then(response => response.json())
-    .then(stocks => {
-      stocks.forEach(stock => {
+    .then(response => {
+  
+      for (i=0; i < 1; i++) {
+        const $div = document.createElement("div")
+        $div.classList.add("carousel-item")
+        $div.classList.add("active")
+        $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
+        $newsCarousel.append($div)
+      }
+    
+      for (i=1; i < 10; i++) {
+        const $div = document.createElement("div")
+        $div.classList.add("carousel-item")
+        $div.innerHTML = `<div id="text-links"><a id="linkers" href="${response.articles[i].url}">${response.articles[i].title}</a></div><img class="d-block w-100 articles" src="${response.articles[i].urlToImage}" alt="First slide" onerror="this.src='./google_news.png'"></img>`
+        $newsCarousel.append($div)
+      }
+  
+      for (i=10; i < 15; i++) {
         const $li = document.createElement("li")
-        $li.classList.add("stock")
-        $li.innerHTML = `${stock.ticker}: $${stock.current_price}`
-        $stockList.append($li)
-      })
+        $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a><div class="headlines-bar"></div>`
+        $headLines.append($li)
+      }
+  
+      for (i=15; i < 16; i++) {
+        const $li = document.createElement("li")
+        $li.innerHTML = `<a id="headlines-linkers" href="${response.articles[i].url}"><strong>${response.articles[i].title}</strong></a>`
+        $headLines.append($li)
+      }
+  
+      for (i=10; i < 15; i++) {
+  
+        const $div = document.createElement('div')
+        $div.classList.add('card')
+        $div.innerHTML = `<img class="card-img-top card-image" src="${response.articles[i].urlToImage}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title article-title">${response.articles[i].title}</h5>
+          <section class="article-button"><a href="${response.articles[i].url}" class="btn btn-primary access">Full article</a><button class="btn btn-primary my-2 my-sm-0 starred" type="submit">⭐</button>
+        </div>`
+        $newsRow.append($div)
+      }
+  
     })
-
-// fetch(imagesUrl)
-//   .then(response => response.json())
-//   .then(images => {
-
   
-//     for (i=0; i < 1; i++) {
-//       const $div = document.createElement("div")
-//       $div.classList.add("carousel-item")
-//       $div.classList.add("active")
-//       $div.innerHTML = `<img class="d-block w-100 images" src="${images[i].url}" alt="First slide"></img>`
-//       $imagesCarousel.append($div)
-//     }
-  
-//     for (i=1; i < 10; i++) {
-//       const $div = document.createElement("div")
-//       $div.classList.add("carousel-item")
-//       $div.innerHTML = `<img class="d-block w-100 images" src="${images[i].url}" alt="First slide"></img>`
-//       $imagesCarousel.append($div)
-//     }
-
-//   })
-
- 
+    fetch(stocksUrl)
+      .then(response => response.json())
+      .then(stocks => {
+        stocks.forEach(stock => {
+          const $li = document.createElement("li")
+          $li.classList.add("stock")
+          $li.innerHTML = `${stock.ticker}: $${stock.current_price}`
+          $stockList.append($li)
+        })
+      })
+}
