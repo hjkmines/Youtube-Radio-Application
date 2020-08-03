713c62a6aaf8bdad1289d59a1f5e24ba4fff1ed7
Tony Kim <hjkmines@gmail.com>
2020-08-02 22:31:54 -0600

    adds form validation

--- Backend/app/models/user.rb
+++ Backend/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, precense: true  
+  validates :last_name, precense: true 
+  validates :email, precense: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, uniqueness: true, length: {in: 7..25}, precense: true
 end
